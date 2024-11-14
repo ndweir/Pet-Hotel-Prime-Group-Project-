3ecd2e458c9cfd3ffb39540ae079cb006f309083
nick <ndweir13@gmail.com>
2024-11-14 10:02:23 -0600

    pet.cs edits

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -2,6 +2,7 @@ using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace pet_hotel
 {
@@ -14,5 +15,26 @@ namespace pet_hotel
         White, Black, Golden, Tricolor, Spotted
 
      }
-    public class Pet {}
+    public class Pet {
+
+        public int id {get; set;}
+
+        [Required]
+        public PetColorType color {get; set;}
+
+        [AllowNull]
+        public DateTime checkedinAt {get; set;}
+
+        public string name {get; set;}
+
+        [Required]
+        public PetBreedType breed {get; set;}
+
+        [Required, ForeignKey("PetOwnerId")]
+        public int PetOwnerId {get; set;}
+
+        
+    }
 }
+
+
